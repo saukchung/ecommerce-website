68bba54aec9e0b0ea7e23ff5fbf2a95c103939f2
Sa Uk Chung <chungsauk95@gmail.com>
2022-07-13 04:12:11 -0400

    Completed the routes for category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -10,21 +10,38 @@ router.get('/', async (req, res) => {
   res.json(categoryData);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  const categoryData =  await Category.findByPk(req.params.id);
   // be sure to include its associated Products
+  res.json(categoryData);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  const categoryData =  await Category.create(req.body);
+  res.send(`Added ${req.body.category_name}`)
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      id: req.body.id,
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      },
+    }
+  )
+  res.send(`Updated category: ${req.params.id}`)
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.delete({where: { id: req.params.id }})
 });
 
 module.exports = router;
