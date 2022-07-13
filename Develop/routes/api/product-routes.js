1d6157307221bfca65bed5ceb536d2944b91baec
Sa Uk Chung <chungsauk95@gmail.com>
2022-07-13 04:14:51 -0400

    Completed the get-by-id and delete routes in product-routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,15 +4,19 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  const productData = await Product.findAll();
   // be sure to include its associated Category and Tag data
+  res.json(productData);
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  const Data =  await Product.findByPk(req.params.id);
   // be sure to include its associated Category and Tag data
+  res.json(Data);
 });
 
 // create new product
@@ -91,6 +95,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.delete({where: { id: req.params.id }})
 });
 
 module.exports = router;
