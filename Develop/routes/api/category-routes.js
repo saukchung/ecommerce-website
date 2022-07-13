9888a59952db0035d3c7a50c199a05d110dbbaf4
Sa Uk Chung <chungsauk95@gmail.com>
2022-07-13 00:26:24 -0400

    Added sequelize connection and the sync to the server.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,9 +3,11 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  const categoryData = await Category.findAll(); 
   // be sure to include its associated Products
+  res.json(categoryData);
 });
 
 router.get('/:id', (req, res) => {
