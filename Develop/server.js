469488b45943121f6ca6f28e4b42de911ab3df49
Sa Uk Chung <chungsauk95@gmail.com>
2022-07-13 00:36:05 -0400

    Changed the sync to function correctly

--- Develop/server.js
+++ Develop/server.js
@@ -12,6 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.sync().listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
+// WAS: 
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
